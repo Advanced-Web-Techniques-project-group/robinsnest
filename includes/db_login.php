e390726c04a18ee9749a7ffb5b99b12dee48b47e
Shnoberts91 <nickroberts1991@hotmail.co.uk>
2017-03-16 12:27:02 +0000

    Dblogin ignore

--- includes/db_login.php
+++ includes/db_login.php
@@ -1,8 +1,8 @@
 <?php 
-  $dbhost  = 'localhost';    // Unlikely to require changing
-  $dbname  = 'robinsnest';   // Modify these...
-  $dbuser  = 'root';   // ...variables according
-  $dbpass  = '8b4ED&g8';   // ...to your installation
+  $dbhost  = 'mudfoot.doc.stu.mmu.ac.uk';    // Unlikely to require changing
+  $dbname  = 'robertsn';   // Modify these...
+  $dbuser  = 'robertsn';   // ...variables according
+  $dbpass  = 'ensteLas6';   // ...to your installation
   $appname = "Robin's Nest"; // ...and preference
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
